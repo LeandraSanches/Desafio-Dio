a61c92c3ed3c2056b2fe276c1774fd6b659cfc74
Leandra <leandra.sanches@hotmail.com>
2022-01-30 17:00:59 -0300

    primeiro commit

---
+++ src/desafioDioArray.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class desafioDioArray {
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+int numeros [] = new int [5];
+int par;
+int impar;
+
+
+       
+         
+     for (int i = 0 ;i < numeros.length ; i = i+1) {
+    	  System.out.println (" Digite um número inteiro");
+          numeros[i] = leitor.nextInt();
+     }
+     
+     System.out.println( "numeros: ")
+         
+         
+              
+     }
+          
+    // if((numeros [i]  % 2) == 0)  {
+        	 //System.out.println(" numeros pares: " + numeros [i]);
+        // }else {
+        	// System.out.println(" numeros impares: " + numeros [i]); 
+        	 
+     }
+         
+          
+         
+         
+        
+     
+       
+        
+        
+    
